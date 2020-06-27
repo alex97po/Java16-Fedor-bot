54d2fcb7da1b73321b5c74326898487f83e698c8
Fedor <dermentlif@gmail.com>
2020-06-27 09:23:14 +0300

    Initial commit

---
+++ src/main/java/com/dermentli/Main.java
@@ -0,0 +1,18 @@
+package com.dermentli;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        System.out.println("GoIT bot started");
+    }
+}
